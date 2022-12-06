8233770af0b7d2eb2621531adad6798c5c49b80a
Nascccom <ostrov_o@bk.ru>
2022-12-06 15:25:36 +0300

    Finished 4 HW!

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -7,7 +7,7 @@ import s from './SuperCheckbox.module.css'
 
 // тип пропсов обычного инпута
 type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>,
-    HTMLInputElement>
+  HTMLInputElement>
 
 type SuperCheckboxPropsType = Omit<DefaultInputPropsType, 'type'> & {
     onChangeChecked?: (checked: boolean) => void
@@ -15,43 +15,45 @@ type SuperCheckboxPropsType = Omit<DefaultInputPropsType, 'type'> & {
 }
 
 const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
-    {
-        onChange,
-        onChangeChecked,
-        className,
-        spanClassName,
-        children, // в эту переменную попадёт текст, типизировать не нужно так как он затипизирован в React.FC
-        id,
+  {
+      onChange,
+      onChangeChecked,
+      className,
+      spanClassName,
+      children, // в эту переменную попадёт текст, типизировать не нужно так как он затипизирован в React.FC
+      id,
 
-        ...restProps // все остальные пропсы попадут в объект restProps
-    }
+      ...restProps // все остальные пропсы попадут в объект restProps
+  }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // задачка на написание онченджа
+        onChangeChecked?.(e.currentTarget.checked)
 
+        onChange?.(e)
     }
 
     const finalInputClassName = s.checkbox
-        + (className ? ' ' + className : '')
+      + (className ? ' ' + className : '')
 
     return (
-        <label className={s.label}>
-            <input
-                id={id}
-                type={'checkbox'}
-                onChange={onChangeCallback}
-                className={finalInputClassName}
-                {...restProps} // отдаём инпуту остальные пропсы если они есть (checked например там внутри)
-            />
-            {children && (
-                <span
-                    id={id ? id + '-span' : undefined}
-                    className={s.spanClassName}
-                >
+      <label className={s.label}>
+          <input
+            id={id}
+            type={'checkbox'}
+            onChange={onChangeCallback}
+            className={finalInputClassName}
+            {...restProps} // отдаём инпуту остальные пропсы если они есть (checked например там внутри)
+          />
+          {children && (
+            <span
+              id={id ? id + '-span' : undefined}
+              className={s.spanClassName}
+            >
                     {children}
                 </span>
-            )}
-        </label> // благодаря label нажатие на спан передастся в инпут
+          )}
+      </label> // благодаря label нажатие на спан передастся в инпут
     )
 }
 
